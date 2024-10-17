e80fd736918d19cf394009767df9f2bf53e643c7
Nixoneczek <patrukskwiel@gmail.com>
2024-10-18 01:42:26 +0200

    Add files via upload
    OTWORZCIE ZA POMOCA 'start.php' spokojnie tu ni ema virusow XDDD

---
+++ Rozwiązanie Problemów.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wybory Prezydenckie</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body><nav class="navbar">
+    <div class="burger-menu" onclick="toggleMenu()">
+        &#9776; <!-- Ikona hamburgera -->
+    </div>
+    <div class="title">
+    <a href="start.php" class="home-link">MOJA KANDYDACJA</a>
+    </div>
+</nav>
+    <div class="menu" id="menu">
+    <ul>
+        <li><span class="menu-item">Nazwa</span></li>
+        <li><a href="start.php">GLOWNA</a></li>
+        <li><a href="#problemy">Rozwiązanie Problemów</a></li>
+        <li><a href="#podatki">Podatki</a></li>
+    </ul>
+</div>
+
+    <div class="content">
+        <div id="nazwa" class="section">
+            <h1><center>(bedzie wybrana)</center></h>
+        </div>
+
+        <div id="problemy" class="section">
+    <h2><center><a href="Rozwiązanie Problemów.php" class="no-underline">Rozwiązanie Problemów</a></center></h2>
+    <p>
+
+<br><b><p>1. Mieszkania dla nowych mieszkańców:</b>
+   <br>- <b>Budowa mieszkań komunalnych:</b> Stwórz bloki mieszkalne lub domy komunalne dla nowych graczy, w których będą mogli zamieszkać na start. Każdy domek powinien mieć 
+   podstawowe wyposażenie: łóżko, piec, skrzynie i crafting table.
+   <br>- <b>Rejonizacja:</b> Wyznacz strefy mieszkalne w różnych częściach miasta. Nowi mieszkańcy mogą otrzymać małe działki, na których będą mogli rozwijać swoje własne domy.
+   <br>- <b>Mieszkania premium</b>: Dla bardziej zaawansowanych graczy możesz zbudować bardziej luksusowe apartamenty w centrum miasta, które mogą kupić za złote sztabki.
+   </p>
+
+   <p>
+   <br><b>2. Rynek publiczny:</b>
+   <br>- <b>Rynek:</b> Zbudujemy razem centralny rynek, gdzie każdy gracz może wynająć stoisko i sprzedawać własne towary, np. jedzenie, materiały budowlane, zbroje lub bronie.
+   <br>- <b>Tablica ogłoszeń:</b> Umieszcze tablicę ogłoszeń na rynku, gdzie gracze mogą zamieszczać oferty handlowe lub wymiany. Moge dodać też zadania zlecone przez miasto, 
+   np. dostarczenie dużej ilości surowców.
+   </p>
+   <p>
+<br><b>3. Kopalnie publiczne:</b>
+<br>- <b>Kopalnie miejskie</b>: Stwórz ogólnodostępne kopalnie, gdzie każdy może wydobywać surowce. Aby zapobiec chaosowi, wyznacz kilka poziomów kopalni: węglową, żelazną i diamentową 
+   (w zależności od głębokości).
+   <br>- <b>Zasady kopania</b>: Wprowadź zasady dotyczące korzystania z kopalni, takie jak zakaz niszczenia ścian czy regularne odnawianie korytarzy, aby kopalnie były bezpieczne i uporządkowane.
+   <br>- <b>Opłaty za korzystanie</b>: Możesz wprowadzić system małych opłat (np. 1 złota sztabka za wstęp), które będą zasilać miejską kasę, a uzyskane środki będą przeznaczane na rozwój miasta.
+   </p>
+   <p>
+<br><b>4. Rozwój infrastruktury</b>:
+<br>- <b>Budowa dróg i mostów</b>: Zbuduj sieć dróg, która połączy kluczowe miejsca w mieście, jak mieszkania, rynek i kopalnie. Może to być także szybki system transportowy z użyciem minecartów.
+<br>- <b>Strefy przemysłowe</b>: Wyznacz miejsca na farmy automatyczne lub przemysłowe (np. farmy trzciny, kaktusów, drzew), które dostarczą materiałów potrzebnych miastu.
+</p>
+<p>
+<br><b>5. System podatków:</b>
+<br>- <b>Podatek od działek</b>: Gracze posiadający działki mogliby płacić mały podatek (np. złote sztabki co tydzień), a zebrane środki mogłyby być przeznaczane na rozwój miasta 
+   i utrzymanie infrastruktury.
+   <br>- <b>Fundusz miejski</b>: Zebrane środki możesz przeznaczyć na rozwój nowych dzielnic, remonty lub publiczne projekty, takie jak wielkie mosty, parki czy monumentalne budowle.
+   </p>
+Dzięki tym rozwiązaniom możesz zapewnić nowym mieszkańcom miejsce do życia, stworzyć dobrze funkcjonujący rynek oraz dostępne dla wszystkich kopalnie.
+ To wszystko pozwoli Twojemu miastu w Minecraft rozwijać się w sposób zorganizowany i przemyślany.
+</div>
+<div class="content">
+        <div id="podatki" class="section">
+            <h2><center>Podatki</center></h2>
+            <b>PODATKI</b> bardzo niskie poniewaz nie ma sensu placic jakiegos suzego nominalu bo i tak ikt bynie placil <b>< 3</b>
+            <br>
+            <br><b>1.</b> za zebrane miesieczne podatki bede ifnansowac miasto oraz staral sie je ulepszac(budowac nowe budynki i kopalnie oraz rynki)
+            <br>
+            <br><b>2.</b> za zaleganie z oplatami miesiecznymi beda naliczane procenty do kolenjnych miesiecy,
+             a za 3 nie oplacone skladki pojawi sie komornik ktory bedzie stopniowo wnynosil twoje rzeczy z domku, skrzynek czy nawet zabieral pojazdy.
+             <br>
+            <br><b>3.</b> waluta nie jest jeszcze ostatecznie ustawiona lecz poczatkowo jest to goldeden ingot. <b>:)</b>
+            <br>
+            <br><center><img src="nig.jpg" width="250px"></center>
+        </div>
+    </div>
+    <script src="script.js"></script>
+    
+</body>
+</html>
---
+++ start.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wybory Prezydenckie</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<nav class="navbar">
+    <div class="burger-menu" onclick="toggleMenu()">
+        &#9776; <!-- Ikona hamburgera -->
+    </div>
+    <div class="title">
+    <a href="start.php" class="home-link">MOJA KANDYDACJA</a>
+    </div>
+</nav>
+
+    <div class="menu" id="menu">
+    <ul>
+        <li><span class="menu-item">Nazwa</span></li>
+        <li><a href="#instytucja">Instytucje</a></li>
+        <li><a href="historia-waluty.php#waluta">waluta</a></li>
+        <li><a href="Rozwiązanie Problemów.php#problemy">Rozwiązanie Problemów</a></li>
+        <li><a href="Rozwiązanie Problemów.php#podatki">Podatki</a></li>
+        <li><a href="Piecza nad handle.php#handle">Piecza nad Handle</a></li>
+        <li><a href="Rozbudowa Mista.php#rozbudowa">Rozbudowa Miasta</a></li>
+    </ul>
+</div>
+
+
+    <div class="content"><center>
+        <h1>Witaj na stronie wyborów prezydenckich!</h1></center>
+        <div id="nazwa" class="section">
+            <h1><center>(bedzie wybrana)</center></h1>
+            <p>wraz z nazwa bedzie opis..</p>
+        </div>
+        <div id="instytucja" class="section">
+            <h2>Instytucje</h2>
+            <p><b>1. Still Water</b>
+   <br>- Reprezentuje tajemnicę i ciszę. Ci, którzy wiedzą, poruszają się w cieniu, jak spokojna woda,
+    nie zdradzając swojego wpływu na wydarzenia. Czasem to, co wydaje się najcichsze, skrywa największe sekrety.
+   </p>
+   <p>
+<br><b>2. Balkan Shitting:</b>
+   <br>-Nawiązuje do surowości i nieprzewidywalności bałkańskich obrzędów. "Balkan Shitting" 
+   to brutalny rytuał oczyszczenia, gdzie tylko najodważniejsi przetrwają, testując swoją wytrzymałość fizyczną i psychiczną.
+   </p>
+<br>
+<p><b>3. Mazowiecki Klekot Jajec:</b>
+   <br>-Symbolizuje siłę i tradycję. To rytuał starych rodów Mazowsza, które mają swoje sekrety przekazywane z pokolenia na pokolenie.
+    "Klekot jaj" to zjawisko, które wskazuje na rozbudzanie mocy, którą dziedziczą tylko nieliczni.
+   </p>
+<br>
+<p><b>4. Those Who Know:</b>
+<br>-To ci, którzy przetrwali te wszystkie próby i zdobyli tajemną wiedzę. Wiedzą, jak manipulować losem,
+ przejść przez spokojne wody, poznać sekrety jaj Mazowsza i przetrwać próby Bałkanów.
+   </p>
+   <br>
+   <b>-Połączenie-</b>
+   <br>To sekretne bractwo mogłoby być zaangażowane w różne intrygi,
+    legendy i starożytne przepowiednie, których członkowie są mistrzami manipulacji i przetrwania. Czujesz, że to ma potencjał?
+        </div>
+        <div id="waluta" class="section">
+    <h2><a href="historia-waluty.php" class="no-underline">Waluta</a></h2>
+    <p>⭡kliknij link⭡
+</div>
+
+<div id="problemy" class="section">
+    <h2><a href="Rozwiązanie Problemów.php" class="no-underline">Rozwiązanie Problemów</a></h2>
+    <p>⭡kliknij link⭡
+        </div>
+
+        <div id="handel" class="section">
+    <h2><a href="Piecza nad Handle.php" class="no-underline">piecza nad handle</a></h2>
+    <p>⭡kliknij link⭡
+</div>
+        <div id="rozbudowa" class="section">
+            <h2><a href="Rozbudowa Mista.php" class="no-underline">Rozbudowa Miasta</a></h2>
+            <p>⭡kliknij link⭡
+        </div>
+    </div>
+    <script src="script.js"></script>
+    
+</body>
+</html>
